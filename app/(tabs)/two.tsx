78d742e8c6ecae6564eb547e808b773bf22d8b30
nishu kumar <kumarnishu437@gmail.com>
2024-11-29 10:27:06 +0530

    commit

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,11 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-paper';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Text>Page 2</Text>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,23 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { useColorScheme } from 'react-native';
 import 'react-native-reanimated';
+import {
+  MD3DarkTheme as DarkTheme,
+  MD3LightTheme as DefaultTheme,
+  ThemeProvider,
+} from 'react-native-paper';
+import Colors from '@/constants/Colors';
 
-import { useColorScheme } from '@/components/useColorScheme';
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+  const [loaded] = useFonts(require('../assets/fonts/SpaceMono-Regular.ttf'));
 
   useEffect(() => {
     if (loaded) {
@@ -46,13 +33,16 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
+  const paperTheme =
+    colorScheme === 'dark'
+      ? { ...DarkTheme, colors: Colors.dark }
+      : { ...DefaultTheme, colors: Colors.light };
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider theme={paperTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal',headerShown:false }} />
       </Stack>
     </ThemeProvider>
   );
